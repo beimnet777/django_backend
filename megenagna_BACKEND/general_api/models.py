23f03f35aa62e067037949344773d9b13f45a216
beimnet777 <91521577+beimnet777@users.noreply.github.com>
2022-05-28 05:17:54 -0700

    the application model is created

--- megenagna_BACKEND/general_api/models.py
+++ megenagna_BACKEND/general_api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from numpy import size
 
 # Create your models here.
 class EmployeeProfile(models.Model):
@@ -15,6 +16,7 @@ class EmployeeProfile(models.Model):
     skills_2=models.CharField(max_length=60)
     skills_3=models.CharField(max_length=60)
     skills_4=models.CharField(max_length=60)
+    sex=models.CharField(max_length=10)
 
 class EmployerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
@@ -24,7 +26,7 @@ class EmployerProfile(models.Model):
     employer_function = models.CharField(max_length = 100)
 
 class Job(models.Model):
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     job_name = models.CharField(max_length= 100)
     job_experience_years = models.IntegerField()
     job_description = models.TextField()
@@ -34,3 +36,10 @@ class Job(models.Model):
     skills_needed_2=models.CharField(max_length=60)
     skills_needed_3=models.CharField(max_length=60)
     skills_needed_4=models.CharField(max_length=60)
+class Application(models.Model):
+    job = models.OneToOneField(Job, on_delete=models.CASCADE, null=True)
+    gpa=models.FloatField(max_length=3)
+    degree=models.CharField(max_length=100)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    sex=models.CharField(max_length=10)
+
